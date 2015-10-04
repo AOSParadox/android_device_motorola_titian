c9f2e88004492c028383f6fc1e7c72e534a009d7
Louis Popi <theh2o64@gmail.com>
2015-10-04 17:05:32 +0200

    falcon : Use timekeep instead of QC Time Daemon
    All credits to https://github.com/sonyxperiadev/timekeep for timekeep binary

--- device_titan.mk
+++ device_titan.mk
@@ -99,6 +99,11 @@ PRODUCT_PACKAGES += \
     power.msm8226 \
     power.qcom
 
+# TimeKeep
+PRODUCT_PACKAGES += \
+    timekeep \
+    TimeKeep
+
 # USB
 PRODUCT_PACKAGES += \
     com.android.future.usb.accessory
